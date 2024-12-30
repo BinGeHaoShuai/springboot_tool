7a799f26e58f740b14e00243f1fadd300ce0a899
ikun <980214152@qq.com>
2024-12-30 14:52:19 +0800

    完善接口

--- src/main/java/com/example/springboot_tool/service/ClaimService.java
+++ src/main/java/com/example/springboot_tool/service/ClaimService.java
@@ -3,6 +3,7 @@ package com.example.springboot_tool.service;
 import java.math.BigInteger;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.springboot_tool.entity.Claim;
 import com.example.springboot_tool.utils.Result;
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.List;
  * @author 君颜未改
  * @since 2024-12-23 23:31:41
  */
-public interface ClaimService {
+public interface ClaimService extends IService<Claim> {
     Claim queryById(BigInteger id);
 
     IPage<Claim> queryByPage(Claim claim, Integer pageNum, Integer pageSize);
